4d8dbfcc40e302584e7ab5e277c2b9af6face88b
IAmNini <fortuny.carrega@gmail.com>
2020-03-24 15:53:32 +0000

    beginning of frontend

---
+++ frontend/components/Quizzes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+const Quizzes = () => {
+  return <>
+  <h1>Just the options of the quizzes you can choose? with links to those components</h1>
+  </>
+}
+
+export default Quizzes
