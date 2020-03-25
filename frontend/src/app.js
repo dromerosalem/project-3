a0aeba61c65f40e7bbcba4a7c1aaf31affa1b420
IAmNini <fortuny.carrega@gmail.com>
2020-03-25 15:44:04 +0000

    questions done

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,6 +7,7 @@ import './style.scss'
 import Login from './components/Login'
 import Register from './components/Register'
 import Quizzes from './components/Quizzes'
+import Question from './components/Question'
 
 const App = () => (
   <BrowserRouter>
@@ -14,6 +15,7 @@ const App = () => (
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/quizzes" component={Quizzes} />
+      <Route exact path="/question" component={Question} />
     </Switch>
   </BrowserRouter>
 )
