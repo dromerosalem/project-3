86b9104107a2ae3b56823f5f08e37b46b33f922e
Alexandra <a.galitzine@gmail.com>
2020-03-25 15:51:55 +0000

    seed

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,9 +3,9 @@ let mongooseHidden = require('mongoose-hidden')()
 const bcrypt = require('bcrypt')
 
 const schema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: { type: String, unique: true },
-  password: { type: String, unique: true }
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true }
 })
 
 schema.plugin(require('mongoose-unique-validator'))
@@ -38,5 +38,5 @@ schema.methods.validatePassword = function validatePassword(password) {
 }
 
 
-module.export = mongoose.model('User', schema)
+module.exports = mongoose.model('User', schema)
 
