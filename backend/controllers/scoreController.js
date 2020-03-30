b9541ebf52528c9db29267c337f0099b6dd27f0b
Alexandra <a.galitzine@gmail.com>
2020-03-30 17:12:07 +0100

    score part 2

--- backend/controllers/scoreController.js
+++ backend/controllers/scoreController.js
@@ -8,16 +8,41 @@ function index(req, res) {
     })
 }
 
+function postToIndex(req, res) {
+  Score 
+    .create(req.body)
+    .then(score => {
+      res.status(201).send(score)
+    })
+}
 
 function getScore(req, res) {
-  // const currentUser = req.currentUser
-  // req.body.user = currentUser
+  const currentUser = req.currentUser
+  req.body.user = currentUser
   Score
-    .findById(req.params.id)
+    .findById(currentUser.id)
     .then(score => {
       res.send(score)
     })
 }
+
+// function edit(req, res) {
+//   // Edit a pancake
+//   const currentUser = req.currentUser
+//   const id = req.params.id
+//   Pancake
+//     .findById(id)
+//     .then(pancake => {
+//       if (!pancake.user.equals(currentUser._id)) return res.status(401).send({ message: 'Unauthorized' })
+//       return pancake.set(req.body)
+//     })
+//     .then(pancake => {
+//       return pancake.save()
+//     })
+//     .then(pancake => {
+//       res.status(202).send(pancake)
+//     })
+// }
 
 function updateScore(req, res) {
   const id = req.params.id
@@ -33,6 +58,7 @@ function updateScore(req, res) {
 
 module.exports = {
   index,
+  postToIndex,
   getScore,
   updateScore
 }
--- backend/models/score.js
+++ backend/models/score.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  score: { type: Number, required: true },
+  gamesPlayed: { type: Number, required: true },
   right: { type: Number, required: true },
   wrong: { type: Number, required: true },
   user: { type: mongoose.Schema.ObjectId, ref: 'User', require: true }
