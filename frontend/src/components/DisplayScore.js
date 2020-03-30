cb24b8c06369dd97b01e13c38a0ffdb8b39e1eee
IAmNini <fortuny.carrega@gmail.com>
2020-03-30 10:00:14 +0100

    day 7

--- frontend/src/components/DisplayScore.js
+++ frontend/src/components/DisplayScore.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const DisplayScore = () => {
   return <>
-  <h1>Good game! Your score is "boh"</h1>
+  <h1>Good game! Your score is ?/10! Nice!</h1>
+  <h2>We&#039;ll fetch your score from our backend when we figure out how to store it there! I bet you can't wait!</h2>
   </>
 }
 
